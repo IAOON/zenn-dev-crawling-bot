6f1fc5c48bc1c222ae77b1cfc39d967ee3d239e3
진대윤 <zetbouaka@gmail.com>
2023-07-26 14:17:25 +0000

    RSS feed 사용하게 변경

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 
 import os
 from mastodon import Mastodon
-from datetime import datetime
+from datetime import timedelta, datetime
+from dateutil import parser
+import requests
+import feedparser
 
 access_token = os.getenv('MASTODON_ACCESS_TOKEN')
 api_base_url = os.getenv('API_BASE_URL')
@@ -12,51 +15,28 @@ mastodon = Mastodon(
     access_token = access_token,
     api_base_url = api_base_url
 )
+
 import requests
-from bs4 import BeautifulSoup
 
-def crawling():
-    url = "https://zenn.dev"
-    response = requests.get(url)
+def feed_crawling():
+    FEED_URL = 'https://zenn.dev/feed' 
 
-    response.encoding = 'utf-8'
+    rss_feed = feedparser.parse(FEED_URL)
 
-    soup = BeautifulSoup(response.text, 'lxml')
+    for entry in rss_feed.entries:
+        # print(entry.keys())
+        parsed_date = parser.parse(entry.published)
+        parsed_date = (parsed_date - timedelta(hours=8)).replace(tzinfo=None) # remove timezone offset
+        now_date = datetime.utcnow()
 
-    base_selector = 'html > body > div:nth-of-type(1) > section:nth-of-type(1) > div > div:nth-of-type(2) > div > div:nth-of-type(1)'
-    additional_selector = 'article > div > a'
+        published_30_minutes_ago = now_date - parsed_date < timedelta(minutes=30)
+        if 1:
+            # send_message(entry.links[0].href)
+            message = f"""
+                Trend Post
+                Title : {entry.title}
+                Link : {entry.links[0].href}
+            """
+            mastodon.toot(message)
 
-    # 기본 CSS selector를 가진 첫번째 요소를 선택
-    base_element = soup.select_one(base_selector)
-
-    if base_element is not None:
-        # 기본 요소 내부에서 추가적인 요소를 선택
-        additional_element = base_element.select_one(additional_selector)
-
-        # 선택된 요소들의 텍스트를 출력
-        print("Base element text: ", base_element.text)
-        if additional_element is not None:
-            data = {"title": additional_element.text, "link": additional_element.get('href')}
-            return data;
-            # print("Additional element text: ", additional_element.text)
-            # 요소의 href 값을 출력
-            # print("Additional element href: ", additional_element.get('href'))
-        else:
-            print("No additional element found within the base element.")
-    else:
-        print("No base element found.")
-    
-try:
-    data = crawling()
-
-    message = f"""
-    Trend Post
-    Title : {data["title"]}
-    Link : {zenn_dev_base_url[:-1]}/{data["link"][1:]}
-    """
-    
-    mastodon.toot(message)
-except Exception as e:
-    print(f"An exception occurred: {e}")
-    mastodon.toot(message)
-
+feed_crawling()
