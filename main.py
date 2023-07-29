c9e3c46ac77b28f290218e12b941b6259073ac60
진대윤 <zetbouaka@gmail.com>
2023-07-29 14:46:13 +0000

    로직 변경에 따른 소스코드 변경

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# !pip3 install feedparser
+# !pip3 install pandas
 #-*- coding: utf-8 -*-
 
 import os
@@ -17,21 +19,53 @@ mastodon = Mastodon(
 )
 
 import requests
+import pandas as pd
+import random
+
+# 배열을 csv 파일로 저장하는 함수
+def export_to_csv(data, filename):
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
+# csv 파일을 읽어와서 데이터를 DataFrame으로 반환하는 함수
+def read_from_csv(filename):
+    try:
+        df = pd.read_csv(filename)
+        # 데이터프레임이 비어있는지 확인
+        if df.empty:
+            return []  
+    except FileNotFoundError:
+        return []
+    except pd.errors.EmptyDataError:
+        return [] 
+    return df
 
 def feed_crawling():
     FEED_URL = 'https://zenn.dev/feed' 
 
     rss_feed = feedparser.parse(FEED_URL)
+    used_items = read_from_csv("used_items.csv")
+
+    data = [] # 데이터 후보_중복이면 체크 안 함
 
     for entry in rss_feed.entries:
-        parsed_date = parser.parse(entry.published)
-        parsed_date = parsed_date.replace(tzinfo=None)
-        now_date = datetime.utcnow()
+        object = {"title":entry.title, "link": entry.links[0].href}
 
-        published_30_minutes_ago = (now_date - parsed_date) < timedelta(minutes=30)
-        if published_30_minutes_ago:
-            message = f"""Trend Post\nTitle : {entry.title}\nLink : {entry.links[0].href}"""
-            # print(message)
-            mastodon.toot(message)
+        if (object in used_items):
+          continue
+        else:
+          data.append(object)
 
+
+    # 무작위로 하나 선택
+    print_entry = random.choice(data)
+
+    message = f"""Trend Post\nTitle : {print_entry["title"]}\nLink : {print_entry["link"]}"""
+    mastodon.toot(message)
+
+    used_items.append({"title":print_entry["title"], "link":{print_entry["link"]}})
+
+    export_to_csv(used_items, "used_items.csv")
+
+export_to_csv([], "used_items.csv")
 feed_crawling()
