460af43a48be4612db9ad37fe7a935b3be6cb6ff
Jaeyeol Lee <rijgndqw012@gmail.com>
2023-08-02 00:47:03 +0900

    피드를 올릴때마다 영어/한국어 번역을 reply로 추가

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 #-*- coding: utf-8 -*-
 
 import os
+import json
+from urllib import parse
+import urllib.request
 from mastodon import Mastodon
 from datetime import timedelta, datetime
 from dateutil import parser
@@ -12,6 +15,9 @@ import feedparser
 access_token = os.getenv('MASTODON_ACCESS_TOKEN')
 api_base_url = os.getenv('API_BASE_URL')
 zenn_dev_base_url = os.getenv('ZENN_DEV_BASE_URL')
+
+NAVER_CLIENT_ID = os.getenv('NAVER_CLIENT_ID')
+NAVER_CLIENT_SECRET = os.getenv('NAVER_CLIENT_SECRET')
 
 mastodon = Mastodon(
     access_token = access_token,
@@ -26,6 +32,29 @@ import random
 def export_to_csv(data, filename):
     df = pd.DataFrame(data)
     df.to_csv(filename, index=False)
+
+def add_translation_reply(reply_to, entry):
+    original_url = entry['link']
+    original_text = entry['title']
+
+    for locale in ["en", "ko"]:
+        data = f"source=ja&target={locale}&text={original_text}"
+        papago_url = f"https://papago.naver.net/website?locale=ko&source=ja&target={locale}&url={parse.quote(original_url)}"
+        url = "https://openapi.naver.com/v1/papago/n2mt"
+        request = urllib.request.Request(url)
+        request.add_header("X-Naver-Client-Id", NAVER_CLIENT_ID)
+        request.add_header("X-Naver-Client-Secret", NAVER_CLIENT_SECRET)
+        response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+        status_code = response.getcode()
+        if(status_code == 200):
+            response_body = response.read()
+            response_body_decoded = response_body.decode('utf-8')
+            result = json.loads(response_body_decoded)['message']['result']['translatedText']
+
+            message = f"""Trend Post\nTitle : {result}\nLink : {papago_url}"""
+            mastodon.status_reply(reply_to, message)
+        else:
+            print("Error Code:" + status_code)
 
 # csv 파일을 읽어와서 데이터를 DataFrame으로 반환하는 함수
 def read_from_csv(filename):
@@ -65,8 +94,9 @@ def feed_crawling():
     print_entry = random.choice(data)
 
     message = f"""Trend Post\nTitle : {print_entry["title"]}\nLink : {print_entry["link"]}"""
-    mastodon.toot(message)
+    reply_to = mastodon.toot(message)
 
+    add_translation_reply(reply_to, print_entry)
     used_items.append({"title":print_entry["title"], "link":{print_entry["link"]}})
 
     export_to_csv(used_items, "used_items.csv")
